ac0ab532881165c03b04cfaf499f9e56abbabee8
amin_ruhul <ruhulinfo50@gmail.com>
2021-05-06 12:35:06 +0600

    fetch data from api

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ const express = require("express");
 var exphbs = require("express-handlebars");
 const app = express();
 const path = require("path");
+const request = require("request");
 
 const PORT = process.env.PORT || 5000;
 const pathname = path.join(__dirname, "public");
+
+const getData = (stockData) => {
+  request(
+    "https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_ceaa65e7fce44e3d8c417d707ca0d5d6",
+    { json: true },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      if (res.statusCode === 200) {
+        console.log(body);
+        //return body;
+        stockData(body);
+      }
+    }
+  );
+};
 
 // set handlebars middleware
 app.engine("handlebars", exphbs());
@@ -12,7 +31,11 @@ app.set("view engine", "handlebars");
 
 // set routes
 app.get("/", function (req, res) {
-  res.render("home");
+  getData((data) => {
+    res.render("home", {
+      stockInfo: data,
+    });
+  });
 });
 app.get("/about", function (req, res) {
   res.render("about");
